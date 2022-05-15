15dbe63b0e4db5dd8eba00f9b54eeaa0cf0be63f
jpcreativeworks <jp.graphics.011@gmail.com>
2022-05-15 15:09:04 -0700

    found one more file to clean

--- server.js
+++ server.js
@@ -11,13 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}!`);
-// });
-//syncs your data base and structures your schemas
-//force tru drops the tables and builds them from scratch, 
+
 sequelize.sync({ force: false }).then(() => {
-  //here doing an app listen, doing what it did before but we are syncing our data bases and then listening for aour express app
   app.listen(PORT, () => console.log('Now listening'));
 });
